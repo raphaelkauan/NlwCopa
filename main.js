f18a08540291ff21916f9a58c12ee8fb429d2002
raphaelkauan <raphaelkauanoficial@gmail.com>
2023-02-20 15:03:12 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+      <img src="/assets/icon-${player1}.svg" />
+      <strong> ${hour} </strong>
+      <img src="/assets/icon-${player2}.svg" />
+  </li>`;
+}
+
+let delay = -0.4;
+function createCard(date, day, games) {
+  delay = delay + 0.4;
+  return `
+  <div class="card" style="animation-delay: ${delay}s">
+          <h2> ${date} <span> ${day} </span> </h2>
+          <ul> ${games} </ul>
+  </div>`;
+}
+
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "sérvia")) +
+  createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
+  createCard("02/12", "sexta", createGame("brazil", "15:00", "japão")) +
+  createCard("02/12", "terça", createGame("colombia", "17:00", "brazil"));
